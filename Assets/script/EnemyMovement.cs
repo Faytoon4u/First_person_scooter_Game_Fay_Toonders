45f93f86f87449930c0c1de40a407c03f4ab63ab
Faytoon4u <144126823+Faytoon4u@users.noreply.github.com>
2023-11-30 10:47:11 +0100

    Initial commit

---
+++ Assets/script/EnemyMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+
+
+{
+    public NavMeshAgent BadGuy;
+    public float squareOfMovement = 20f;
+    private float xMin;
+    private float xMax;
+
+    private float zMin;
+    private float zMax;
+
+    private float xPosition;
+    private float zPosition;
+    private float yPosition;
+
+    public float CloseEnough;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        xMin = -squareOfMovement;
+        zMin = -squareOfMovement;
+        xMax = squareOfMovement;
+        zMax = squareOfMovement;
+
+        newLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, new Vector3(xPosition, yPosition, zPosition)) <= CloseEnough) //makes it go to a new location if its close enough or at the other loc
+        {
+            newLocation();
+        }
+
+    }
+
+    public void newLocation()
+    { 
+        yPosition = transform.position.y;
+        xPosition = Random.Range(xMin, xMax);
+        zPosition = Random.Range(zMin, zMax);
+
+        BadGuy.SetDestination(new Vector3(xPosition, yPosition, zPosition));
+    }
+
+}
---
+++ Assets/script/shooting.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public Camera cam;
+
+
+    private Ray ray;
+    private RaycastHit hit;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // this is the right mouse klik
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray,out hit))
+            {
+               if (hit.collider.tag.Equals("npc"))
+               {
+                    Destroy(hit.collider.gameObject); // the GameObject gets destroyed here
+                    Debug.Log("hit");
+               }
+            }
+            
+        }
+    }
+}
